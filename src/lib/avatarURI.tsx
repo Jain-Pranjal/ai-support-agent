0751316db6e403c9765be48f460faa399a4824ab
Pranjal Jain <pranjalworkon@gmail.com>
2025-12-21 22:31:06 +0530

    feat: added avatar for UI

---
+++ src/lib/avatarURI.tsx
@@ -0,0 +1,30 @@
+import { createAvatar } from '@dicebear/core'
+import { rings, openPeeps } from '@dicebear/collection'
+
+interface AvatarProps {
+    seed: string
+    variant: 'rings' | 'openPeeps'
+}
+
+export const generatedAvatarURI = ({ seed, variant }: AvatarProps) => {
+    let avatar
+
+    if (variant === 'rings') {
+        avatar = createAvatar(rings, {
+            seed,
+        })
+    } else if (variant === 'openPeeps') {
+        avatar = createAvatar(openPeeps, {
+            seed,
+            size: 48,
+        })
+    } else {
+        throw new Error('Invalid avatar variant')
+    }
+
+    return avatar.toDataUri()
+}
+
+//this will be used to generate the avatar URI based on the seed and variant provide for the Oration AI Chatbot
+
+// this dataURI can be directly used in the src attribute of an img tag
